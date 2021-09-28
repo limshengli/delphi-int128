7199865e329d74549d3f591ca8aef3adeb80bb58
Lim Sheng Li <limshengli@sql.com.my>
2021-09-28 09:29:10 +0800

    Added support for addition operators.

--- Test/int128Impl.TestCase.pas
+++ Test/int128Impl.TestCase.pas
@@ -17,6 +17,16 @@ type
     procedure Test_Int64;
     procedure Test_UInt64;
     procedure Test_Int128;
+    procedure Test_String_Convert;
+    procedure Test_Addition;
+    procedure Test_Subtraction;
+    procedure Test_Multiplication;
+    procedure Test_Division;
+    procedure Test_Left_Shift;
+    procedure Test_Right_Shift;
+    procedure Test_Modulus;
+    procedure Test_Less_Than;
+    procedure Test_Greater_Than;
   end;
 
 implementation
@@ -154,6 +164,168 @@ begin
   A := High(UInt64) shr 1;
   A := A * High(UInt64);
   CheckEquals('170141183460469231704017187605319778305', A);
+end;
+
+procedure TInt128_TestCase.Test_String_Convert;
+begin
+  var A: Int128;
+
+  A := Int128('-3544231');
+  CheckEquals('-3544231', A);
+  CheckTrue(A = Int128('-3544231'));
+
+  A := Int128('41684684167684656548645814468');
+  CheckEquals('41684684167684656548645814468', A);
+  CheckTrue(A = Int128('41684684167684656548645814468'));
+
+  A := Int128('-5465846554342544511323446554342136574');
+  CheckEquals('-5465846554342544511323446554342136574', A);
+  CheckFalse(A = Int128('-5465846554342544511323446554342136573'));
+
+  A := Int128('5686');
+  CheckEquals('5686', A);
+  CheckFalse(A = Int128('-566'));
+end;
+
+procedure TInt128_TestCase.Test_Addition;
+begin
+  var A, B: Int128;
+  A := Int128('2514468541324685313585343515684');
+  B := Int128('5231458413545231584365135435132');
+
+  A := A + B;
+  CheckEquals('7745926954869916897950478950816', A);
+
+  B := Int128('-546841354751434768845435113558');
+  A := B + A;
+  CheckEquals('7199085600118482129105043837258', A);
+end;
+
+procedure TInt128_TestCase.Test_Subtraction;
+begin
+  var A, B: Int128;
+  A := Int128('5436854135856841354184345341532435123');
+  B := Int128('5634468412534683453548654135154685433');
+
+  A := A - B;
+  CheckEquals('-197614276677842099364308793622250310', A);
+
+  B := Int128('6546854355185413546841514354685431');
+  A := B - A;
+  CheckEquals('204161131033027512911150307976935741', A);
+end;
+
+procedure TInt128_TestCase.Test_Multiplication;
+begin
+  var A: Int128;
+  A := Int128('3564643518354354134854163485');
+
+  A := A * 398;
+  CheckEquals('1418728120305032945671957067030', A);
+
+  A := A * -9536;
+  CheckEquals('-13528991355228794169927782591198080', A);
+end;
+
+procedure TInt128_TestCase.Test_Division;
+begin
+  var A: Int128;
+  A := Int128('54168545353251854354351545464685485554');
+
+  A := A div 5468945;
+  CheckEquals('9904752260856866242822252822927', A);
+
+  A := A div 5959;
+  CheckEquals('1662150068947284148820649911', A);
+
+  A := A div -100;
+  CheckEquals('-16621500689472841488206500', A);
+end;
+
+procedure TInt128_TestCase.Test_Left_Shift;
+var C: Int128;
+begin
+  C := Int128(123) shl 100;
+  CheckEquals('155921023828072216384094494261248', C);
+
+  C := Int128(-1234) shl 40;
+  CheckEquals('-1356797348675584', C);
+
+  C := Int128('534534684685') shl 3;
+  CheckEquals('4276277477480', C);
+
+//  C := Int128(123) shl -1;
+//  CheckEquals('61', C);
+//
+//  C := Int128(123) shl -100;
+//  CheckEquals('0', C);
+
+  C := Int128(19632) shl 128;
+  CheckEquals('0', C);
+
+//  C := Int128('-123548689745632458638471127834758368') shl -100;
+//  CheckEquals('-97462', C);
+end;
+
+procedure TInt128_TestCase.Test_Right_Shift;
+var C: Int128;
+begin
+  C := Int128('125794368745377835274623412345424') shr 100;
+  CheckEquals('99', C);
+
+  C := Int128('1257943687453778352746') shr 38;
+  CheckEquals('4576372475', C);
+
+  C := Int128('1257943687453778352746') shr 5;
+  CheckEquals('39310740232930573523', C);
+
+  C := Int128(-100) shr 1;
+  CheckEquals('-50', C);
+
+//  C := Int128(5566) shr -96;
+//  CheckEquals('440983952554395303045665627570176', C);
+
+  C := Int128(51681) shr 128;
+  CheckEquals('0', C);
+
+//  C := Int128(-123) shr -100;
+//  CheckEquals('-155921023828072216384094494261248', C);
+end;
+
+procedure TInt128_TestCase.Test_Modulus;
+var A: Int128;
+begin
+  A := Int128('356478543547684735413847686485341');
+  A := A mod 54685466;
+  CheckEquals('12345495', A);
+
+  A := Int128('35846854351638685354684365');
+  A := A mod 354654;
+  CheckEquals('101013', A);
+end;
+
+procedure TInt128_TestCase.Test_Less_Than;
+begin
+  var A := Int128('523216854135135468461354685314');
+  var B := Int128('-546854135847865413548435453435');
+  CheckTrue(B < A);
+  CheckFalse(A < B);
+
+  B := Int128('534584135413543136541351');
+  CheckTrue(B < A);
+  CheckFalse(A < B);
+end;
+
+procedure TInt128_TestCase.Test_Greater_Than;
+begin
+  var A := Int128('86415374685435153748614385455744441');
+  var B := Int128('5343513218541513354468435135843658');
+  CheckTrue(A > B);
+  CheckFalse(B > A);
+
+  B := Int128('-545613546854135213854765332158132106');
+  CheckTrue(A > B);
+  CheckFalse(B > A);
 end;
 
 initialization
--- int128impl.pas
+++ int128impl.pas
@@ -10,6 +10,10 @@ type
         out DivResult: Int128; out Remainder: Int128); static;
     class procedure Inc128(var Value: Int128); static;
     class procedure SetBit128(var Value: Int128; numBit: integer); static;
+    class procedure UnsetBit128(var Value: Int128; numBit: integer); static;
+    class function GetBit128(var Value: Int128; numBit: integer): integer; static;
+    class procedure ToggleBit128(var Value: Int128; numBit: integer); static;
+    class function StrToInt128(Value: String): Int128; static;
     function Invert: Int128;
   public
     class operator Add(a, b: Int128): Int128;
@@ -25,22 +29,28 @@ type
     class operator Implicit(Value: Int64): Int128;
     class operator Implicit(Value: UInt64): Int128;
     class operator Implicit(Value: Int128): string;
-    class operator LeftShift(Value: Int128; Shift: Integer): Int128;
+    class operator Implicit(Value: string): Int128;
+    class operator LeftShift(Value: Int128; Shift: Byte): Int128;
     class operator LeftShift(Value, Shift: Int128): Int128;
     class operator LessThan(a, b: Int128): Boolean;
     class operator LessThanOrEqual(a, b: Int128): Boolean;
     class operator LogicalNot(Value: Int128): Int128;
     class operator Multiply(a, b: Int128): Int128;
     class operator NotEqual(a, b: Int128): Boolean;
-    class operator RightShift(Value: Int128; Shift: Integer): Int128;
+    class operator RightShift(Value: Int128; Shift: Byte): Int128;
     class operator RightShift(Value, Shift: Int128): Int128;
     class operator Subtract(a, b: Int128): Int128;
+    class operator IntDivide(a, b: Int128): Int128;
+    class operator Modulus(a, b: Int128): Int128;
+    class operator BitwiseOr(a, b: Int128): Int128;
+    class operator BitwiseXor(a, b: Int128): Int128;
+    class operator BitwiseAnd(a, b: Int128): Int128;
   strict private
     case Byte of
-      0: (sb: packed array[0..15] of UInt8);
-      1: (db: packed array[0..7] of UInt16);
-      2: (dw0, dw1, dw2, dw3: UInt32);
-      3: (dw: packed array[0..3] of UInt32);
+      0: (b: packed array[0..15] of UInt8);
+      1: (w: packed array[0..7] of UInt16);
+      2: (c0, c1, c2, c3: UInt32);
+      3: (c: packed array[0..3] of UInt32);
       4: (dc0, dc1: UInt64);
       5: (dc: packed array[0..1] of UInt64);
   end;
@@ -54,7 +64,7 @@ class procedure Int128.DivMod128(const Value1: Int128; const Value2: Int128;
 var curShift: integer;
     sub: Int128;
 begin
-  if Value2 = 0 then raise Exception.Create('DivMod128: div by zero');
+  if Value2 = 0 then raise EMathError.Create('DivMod128: div by zero');
   if Value2 = 1 then begin
     DivResult := Value1;
     Remainder := 0;
@@ -65,7 +75,7 @@ begin
   DivResult := 0;
 
   curShift := 0;
-  while (sub.dw3 and $80000000 = 0) and (sub < Remainder) do begin
+  while (sub.c3 and $80000000 = 0) and (sub < Remainder) do begin
     sub := sub shl 1;
     inc(curShift);
     if (sub > Remainder) then begin
@@ -91,14 +101,14 @@ end;
 
 class procedure Int128.Inc128(var Value: Int128);
 begin
-  if Value.dw0 <> $ffffffff then Inc(Value.dw0) else begin
-    Value.dw0 := 0;
-    if Value.dw1 <> $ffffffff then Inc(Value.dw1) else begin
-      Value.dw1 := 0;
-      if Value.dw2 <> $ffffffff then Inc(Value.dw2) else begin
-        Value.dw2 := 0;
-        if Value.dw3 <> $ffffffff then Inc(Value.dw3) else
-          Value.dw3 := 0;
+  if Value.c0 <> $ffffffff then Inc(Value.c0) else begin
+    Value.c0 := 0;
+    if Value.c1 <> $ffffffff then Inc(Value.c1) else begin
+      Value.c1 := 0;
+      if Value.c2 <> $ffffffff then Inc(Value.c2) else begin
+        Value.c2 := 0;
+        if Value.c3 <> $ffffffff then Inc(Value.c3) else
+          Value.c3 := 0;
       end;
     end;
   end;
@@ -106,7 +116,43 @@ end;
 
 class procedure Int128.SetBit128(var Value: Int128; numBit: integer);
 begin
-  Value.dw[numBit shr 5] := Value.dw[numBit shr 5] or longword(1 shl (numBit and 31));
+  Value.c[numBit shr 5] := Value.c[numBit shr 5] or longword(1 shl (numBit and 31));
+end;
+
+class procedure Int128.UnsetBit128(var Value: Int128; numBit: Integer);
+begin
+  Value.c[numBit shr 5] := Value.c[numBit shr 5] and not longword(1 shl (numBit and 31));
+end;
+
+class function Int128.GetBit128(var Value: Int128; numBit: Integer): integer;
+begin
+  Result := (Value.c[numBit shr 5] shr (numBit and 31)) and 1;
+end;
+
+class procedure Int128.ToggleBit128(var Value: Int128; numBit: Integer);
+begin
+  Value.c[numBit shr 5] := Value.c[numBit shr 5] xor longword(1 shl (numBit and 31));
+end;
+
+class function Int128.StrToInt128(Value: string): Int128;
+var
+  i: Integer;
+  ten: Int128;
+  neg: Boolean;
+begin
+  Result := 0;
+  ten := 10;
+  neg := False;
+  for i := 1 to length(Value) do begin
+    if Value[i] in ['0'..'9'] then begin
+      Result := Result * ten;
+      Result := Result + Int32(Ord(Value[i]) - Ord('0'));
+    end else if (i = 1) and (Value[i] = '-') then
+      neg := True
+    else
+      raise EConvertError.Create(Value + ' is not a valid Int128 value.');
+  end;
+  if neg then Result := Result * -1;
 end;
 
 function Int128.Invert: Int128;
@@ -121,48 +167,48 @@ var qw: UInt64;
 
   procedure inc3;
   begin
-    if Result.dw3 = $ffffffff then
+    if Result.c3 = $ffffffff then
     begin
-      Result.dw3 := 0;
+      Result.c3 := 0;
     end else
-      Inc(Result.dw3);
+      Inc(Result.c3);
   end;
 
   procedure inc2;
   begin
-    if Result.dw2 = $ffffffff then
+    if Result.c2 = $ffffffff then
     begin
-      Result.dw2 := 0;
+      Result.c2 := 0;
       inc3;
     end else
-      Inc(Result.dw2);
+      Inc(Result.c2);
   end;
 
   procedure inc1;
   begin
-    if Result.dw1 = $ffffffff then
+    if Result.c1 = $ffffffff then
     begin
-      Result.dw1 := 0;
+      Result.c1 := 0;
       inc2;
     end else
-      Inc(Result.dw1);
+      Inc(Result.c1);
   end;
 
 begin
-  qw := UInt64(a.dw0) + UInt64(b.dw0);
-  Result.dw0 := qw and $ffffffff;
+  qw := UInt64(a.c0) + UInt64(b.c0);
+  Result.c0 := qw and $ffffffff;
   c0 := (qw shr 32) = 1;
 
-  qw := UInt64(a.dw1) + UInt64(b.dw1);
-  Result.dw1 := qw and $ffffffff;
+  qw := UInt64(a.c1) + UInt64(b.c1);
+  Result.c1 := qw and $ffffffff;
   c1 := (qw shr 32) = 1;
 
-  qw := UInt64(a.dw2) + UInt64(b.dw2);
-  Result.dw2 := qw and $ffffffff;
+  qw := UInt64(a.c2) + UInt64(b.c2);
+  Result.c2 := qw and $ffffffff;
   c2 := (qw shr 32) = 1;
 
-  qw := UInt64(a.dw3) + UInt64(b.dw3);
-  Result.dw3 := qw and $ffffffff;
+  qw := UInt64(a.c3) + UInt64(b.c3);
+  Result.c3 := qw and $ffffffff;
 
   if c0 then inc1;
   if c1 then inc2;
@@ -171,18 +217,14 @@ end;
 
 class operator Int128.Equal(a, b: Int128): Boolean;
 begin
-  Result := (a.dw0 = b.dw0) and (a.dw1 = b.dw1) and (a.dw2 = b.dw2) and (a.dw3 = b.dw3);
+  Result := (a.dc0 = b.dc0) and (a.dc1 = b.dc1);
 end;
 
 class operator Int128.GreaterThan(a, b: Int128): Boolean;
 begin
-  if a.dw3 > b.dw3 then Result := True
-  else if a.dw3 < b.dw3 then Result := False
-  else if a.dw2 > b.dw2 then Result := True
-  else if a.dw2 < b.dw2 then Result := False
-  else if a.dw1 > b.dw1 then Result := True
-  else if a.dw1 < b.dw1 then Result := False
-  else if a.dw0 > b.dw0 then Result := True
+  if a.dc1 > b.dc1 then Result := True
+  else if a.dc1 < b.dc1 then Result := False
+  else if a.dc0 > b.dc0 then Result := True
   else Result := False;
 end;
 
@@ -194,46 +236,46 @@ end;
 class operator Int128.Implicit(Value: Int8): Int128;
 begin
   var Sign: Byte := 0;
-  Result.sb[0] := Value;
+  Result.b[0] := Value;
   if Value < 0 then Sign := $FF;
 
-  FillChar(Result.sb[1], SizeOf(Result) - SizeOf(Result.sb[0]), Sign);
+  FillChar(Result.b[1], SizeOf(Result) - SizeOf(Result.b[0]), Sign);
 end;
 
 class operator Int128.Implicit(Value: UInt8): Int128;
 begin
-  Result.sb[0] := Value;
-  FillChar(Result.sb[1], SizeOf(Result) - SizeOf(Result.sb[0]), 0);
+  Result.b[0] := Value;
+  FillChar(Result.b[1], SizeOf(Result) - SizeOf(Result.b[0]), 0);
 end;
 
 class operator Int128.Implicit(Value: Int16): Int128;
 begin
   var Sign: Byte := 0;
-  Result.db[0] := Value;
+  Result.w[0] := Value;
   if Value < 0 then Sign := $FF;
 
-  FillChar(Result.db[1], SizeOf(Result) - SizeOf(Result.db[0]), Sign);
+  FillChar(Result.w[1], SizeOf(Result) - SizeOf(Result.w[0]), Sign);
 end;
 
 class operator Int128.Implicit(Value: UInt16): Int128;
 begin
-  Result.db[0] := Value;
-  FillChar(Result.db[1], SizeOf(Result) - SizeOf(Result.db[0]), 0);
+  Result.w[0] := Value;
+  FillChar(Result.w[1], SizeOf(Result) - SizeOf(Result.w[0]), 0);
 end;
 
 class operator Int128.Implicit(Value: Int32): Int128;
 begin
   var Sign: Byte := 0;
-  Result.dw[0] := Value;
+  Result.c[0] := Value;
   if Value < 0 then Sign := $FF;
 
-  FillChar(Result.dw[1], SizeOf(Result) - SizeOf(Result.dw[0]), Sign);
+  FillChar(Result.c[1], SizeOf(Result) - SizeOf(Result.c[0]), Sign);
 end;
 
 class operator Int128.Implicit(Value: UInt32): Int128;
 begin
-  Result.dw[0] := Value;
-  FillChar(Result.dw[1], SizeOf(Result) - SizeOf(Result.dw[0]), 0);
+  Result.c[0] := Value;
+  FillChar(Result.c[1], SizeOf(Result) - SizeOf(Result.c[0]), 0);
 end;
 
 class operator Int128.Implicit(Value: Int64): Int128;
@@ -257,7 +299,7 @@ var digit, curValue, nextValue, ten: Int128;
 begin
   Result := '';
   ten := 10;
-  if Value.sb[15] shr 7 = 1 then begin
+  if Value.b[15] shr 7 = 1 then begin
     curValue := Value.Invert + 1;
     Neg := True;
   end else begin
@@ -266,58 +308,53 @@ begin
   end;
   while CurValue <> 0 do begin
     DivMod128(CurValue, ten, nextValue, digit);
-    Result := Chr(Ord('0') + digit.dw0) + Result;
+    Result := Chr(Ord('0') + digit.c0) + Result;
     curValue := NextValue;
   end;
   if Result = '' then Result := '0';
   if Neg then Result := '-' + Result;
 end;
 
-class operator Int128.LeftShift(Value: Int128; Shift: Integer): Int128;
+class operator Int128.Implicit(Value: string): Int128;
+begin
+  Result := StrToInt128(Value);
+end;
+
+class operator Int128.LeftShift(Value: Int128; Shift: Byte): Int128;
 begin
   if Shift >= 128 then
     Result := 0
   else if Shift >= 64 then begin
-    Result.dw2 := Value.dw0;
-    Result.dw3 := Value.dw1;
-    Result.dw1 := 0;
-    Result.dw0 := 0;
+    Result.dc1 := Value.dc0;
+    Result.dc0 := 0;
     Result := Result shl (Shift - 64);
   end else if Shift >= 32 then begin
-    Result.dw3 := Value.dw2;
-    Result.dw2 := Value.dw1;
-    Result.dw1 := Value.dw0;
-    Result.dw0 := 0;
+    Result.dc1 := (Value.dc1 shl 32) or (Value.dc0 shr 32);
+    Result.dc0 := (Value.dc0 shl 32);
     Result := Result shl (Shift - 32);
   end else if Shift = 0 then
     Result := Value
   else if Shift < 0 then
     Result := Value shr (-shift)
   else begin
-    Result.dw3 := (Value.dw3 shl Shift) or (Value.dw2 shr (32-shift));
-    Result.dw2 := (Value.dw2 shl Shift) or (Value.dw1 shr (32-shift));
-    Result.dw1 := (Value.dw1 shl Shift) or (Value.dw0 shr (32-shift));
-    Result.dw0 := (Value.dw0 shl Shift);
+    Result.dc1 := (Value.dc1 shl Shift) or (Value.dc0 shr (64-shift));
+    Result.dc0 := (Value.dc0 shl Shift);
   end;
 end;
 
 class operator Int128.LeftShift(Value, Shift: Int128): Int128;
 begin
-  if (Shift.dw0 > 128) or (Shift.dw1 <> 0) or (Shift.dw2 <> 0) or (Shift.dw3 <> 0) then
+  if (Shift.c0 > 128) or (Shift.c1 <> 0) or (Shift.c2 <> 0) or (Shift.c3 <> 0) then
     Result := 0
   else
-    Result := Value shl Shift.dw0;
+    Result := Value shl Shift.b[0];
 end;
 
 class operator Int128.LessThan(a, b: Int128): Boolean;
 begin
-  if a.dw3 < b.dw3 then Result := True
-  else if a.dw3 > b.dw3 then Result := False
-  else if a.dw2 < b.dw2 then Result := True
-  else if a.dw2 > b.dw2 then Result := False
-  else if a.dw1 < b.dw1 then Result := True
-  else if a.dw1 > b.dw1 then Result := False
-  else if a.dw0 < b.dw0 then Result := True
+  if a.dc1 < b.dc1 then Result := True
+  else if a.dc1 > b.dc1 then Result := False
+  else if a.dc0 < b.dc0 then Result := True
   else Result := False;
 end;
 
@@ -328,83 +365,81 @@ end;
 
 class operator Int128.LogicalNot(Value: Int128): Int128;
 begin
-  Result.dw0 := not Value.dw0;
-  Result.dw1 := not Value.dw1;
-  Result.dw2 := not Value.dw2;
-  Result.dw3 := not Value.dw3;
+  Result.dc0 := not Value.dc0;
+  Result.dc1 := not Value.dc1;
 end;
 
 class operator Int128.Multiply(a, b: Int128): Int128;
 var qw: UInt64;
     v: Int128;
 begin
-  qw := UInt64(a.dw0) * UInt64(b.dw0);
-  Result.dw0 := qw and $ffffffff;
-  Result.dw1 := qw shr 32;
-  Result.dw2 := 0;
-  Result.dw3 := 0;
+  qw := UInt64(a.c0) * UInt64(b.c0);
+  Result.c0 := qw and $ffffffff;
+  Result.c1 := qw shr 32;
+  Result.c2 := 0;
+  Result.c3 := 0;
 
-  qw := UInt64(a.dw0) * UInt64(b.dw1);
-  v.dw0 := 0;
-  v.dw1 := qw and $ffffffff;
-  v.dw2 := qw shr 32;
-  v.dw3 := 0;
+  qw := UInt64(a.c0) * UInt64(b.c1);
+  v.c0 := 0;
+  v.c1 := qw and $ffffffff;
+  v.c2 := qw shr 32;
+  v.c3 := 0;
   Result := Result + v;
 
-  qw := UInt64(a.dw1) * UInt64(b.dw0);
-  v.dw0 := 0;
-  v.dw1 := qw and $ffffffff;
-  v.dw2 := qw shr 32;
-  v.dw3 := 0;
+  qw := UInt64(a.c1) * UInt64(b.c0);
+  v.c0 := 0;
+  v.c1 := qw and $ffffffff;
+  v.c2 := qw shr 32;
+  v.c3 := 0;
   Result := Result + v;
 
-  qw := UInt64(a.dw0) * UInt64(b.dw2);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := qw and $ffffffff;
-  v.dw3 := qw shr 32;
+  qw := UInt64(a.c0) * UInt64(b.c2);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := qw and $ffffffff;
+  v.c3 := qw shr 32;
   Result := Result + v;
 
-  qw := UInt64(a.dw1) * UInt64(b.dw1);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := qw and $ffffffff;
-  v.dw3 := qw shr 32;
+  qw := UInt64(a.c1) * UInt64(b.c1);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := qw and $ffffffff;
+  v.c3 := qw shr 32;
   Result := Result + v;
 
-  qw := UInt64(a.dw2) * UInt64(b.dw0);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := qw and $ffffffff;
-  v.dw3 := qw shr 32;
+  qw := UInt64(a.c2) * UInt64(b.c0);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := qw and $ffffffff;
+  v.c3 := qw shr 32;
   Result := Result + v;
 
-  qw := UInt64(a.dw0) * UInt64(b.dw3);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := 0;
-  v.dw3 := qw and $ffffffff;
+  qw := UInt64(a.c0) * UInt64(b.c3);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := 0;
+  v.c3 := qw and $ffffffff;
   Result := Result + v;
 
-  qw := UInt64(a.dw1) * UInt64(b.dw2);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := 0;
-  v.dw3 := qw and $ffffffff;
+  qw := UInt64(a.c1) * UInt64(b.c2);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := 0;
+  v.c3 := qw and $ffffffff;
   Result := Result + v;
 
-  qw := UInt64(a.dw2) * UInt64(b.dw1);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := 0;
-  v.dw3 := qw and $ffffffff;
+  qw := UInt64(a.c2) * UInt64(b.c1);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := 0;
+  v.c3 := qw and $ffffffff;
   Result := Result + v;
 
-  qw := UInt64(a.dw3) * UInt64(b.dw0);
-  v.dw0 := 0;
-  v.dw1 := 0;
-  v.dw2 := 0;
-  v.dw3 := qw and $ffffffff;
+  qw := UInt64(a.c3) * UInt64(b.c0);
+  v.c0 := 0;
+  v.c1 := 0;
+  v.c2 := 0;
+  v.c3 := qw and $ffffffff;
   Result := Result + v;
 end;
 
@@ -413,42 +448,37 @@ begin
   Result := not (a = b);
 end;
 
-class operator Int128.RightShift(Value: Int128; Shift: Integer): Int128;
+class operator Int128.RightShift(Value: Int128; Shift: Byte): Int128;
+var neg: Int128;
 begin
+  neg := Value;
+
   if Shift >= 128 then
     Result := 0
-  else if Shift >= 64 then
-  begin
-    Result.dw0 := Value.dw2;
-    Result.dw1 := Value.dw3;
-    Result.dw2 := 0;
-    Result.dw3 := 0;
+  else if Shift >= 64 then begin
+    Result.dc0 := Value.dc1;
+    Result.dc1 := 0;
     Result := Result shr (Shift - 64);
-  end else if Shift >= 32 then
-  begin
-    Result.dw0 := Value.dw1;
-    Result.dw1 := Value.dw2;
-    Result.dw2 := Value.dw3;
-    Result.dw3 := 0;
+  end else if Shift >= 32 then begin
+    Result.dc0 := (Value.dc0 shr 32) or (Value.dc1 shl 32);
+    Result.dc1 := (Value.dc1 shr 32);
     Result := Result shr (Shift - 32);
   end else if Shift = 0 then
-    Result := Value else
-  if Shift < 0 then
+    Result := Value
+  else if Shift < 0 then
     Result := Value shl (-shift)
-  else
-  begin
-    Result.dw0 := (Value.dw0 shr Shift) or (Value.dw1 shl (32-shift));
-    Result.dw1 := (Value.dw1 shr Shift) or (Value.dw2 shl (32-shift));
-    Result.dw2 := (Value.dw2 shr Shift) or (Value.dw3 shl (32-shift));
-    Result.dw3 := (Value.dw3 shr Shift);
+  else begin
+    Result.dc0 := (Value.dc0 shr Shift) or (Value.dc1 shl (64-Shift));
+    Result.dc1 := (Value.dc1 shr Shift);
   end;
 end;
 
 class operator Int128.RightShift(Value, Shift: Int128): Int128;
 begin
-  if (Shift.dw0 > 128) or (Shift.dw1 <> 0) or (Shift.dw2 <> 0)
-    or (Shift.dw3 <> 0) then Result := 0 else
-    Result := Value shr Shift.dw0;
+  if (Shift.c0 > 128) or (Shift.c1 <> 0) or (Shift.c2 <> 0) or (Shift.c3 <> 0) then
+    Result := 0
+  else
+    Result := Value shr Shift.b[0];
 end;
 
 class operator Int128.Subtract(a, b: Int128): Int128;
@@ -457,6 +487,36 @@ begin
   c := not b;
   Inc128(c);
   Result := a + c;
+end;
+
+class operator Int128.IntDivide(a, b: Int128): Int128;
+var temp: Int128;
+begin
+  DivMod128(a, b, Result, temp);
+end;
+
+class operator Int128.Modulus(a: Int128; b: Int128): Int128;
+var temp: Int128;
+begin
+  DivMod128(a, b, temp, Result);
+end;
+
+class operator Int128.BitwiseOr(a, b: Int128): Int128;
+begin
+  Result.dc0 := a.dc0 or b.dc0;
+  Result.dc1 := a.dc1 or b.dc1;
+end;
+
+class operator Int128.BitwiseXor(a, b: Int128): Int128;
+begin
+  Result.dc0 := a.dc0 xor b.dc0;
+  Result.dc1 := a.dc1 xor b.dc1;
+end;
+
+class operator Int128.BitwiseAnd(a, b: Int128): Int128;
+begin
+  Result.dc0 := a.dc0 and b.dc0;
+  Result.dc1 := a.dc1 and b.dc1;
 end;
 
 end.
